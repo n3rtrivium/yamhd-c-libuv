31267822c93773b743c8eaaf5342270a7e2a10cd
Martin Wind <mawi1988@gmail.com>
2015-02-06 18:24:11 +0100

    fix copyright

--- yamhd-c-libuv.c
+++ yamhd-c-libuv.c
@@ -1,5 +1,5 @@
 //
-//     (c) 2014 Martin Wind and Rene Romanus
+//     (c) 2014-2015 Martin Wind and Rene Romanus
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
